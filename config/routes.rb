20c47c136c69f4d0618058bae683233d94424d9f
ChoJaewoo <chjw102@gmail.com>
2013-02-26 17:04:07 +0900

    Finish Sign in

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Microblog::Application.routes.draw do
   root to: "static_pages#home"
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
+  match "signin", to: "sessions#new"
+  match "signout", to: "sessions#destroy", via: :delete
   match "signup", to: "users#new"
 
   match "help", to: 'static_pages#help'
