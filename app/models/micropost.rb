221233dccfa622d517da9f4908be1d64a49e2212
ChoJaewoo <chjw102@gmail.com>
2013-03-02 12:03:40 +0900

    micropost

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,10 @@
 class Micropost < ActiveRecord::Base
-  attr_accessible :content, :user_id
+  attr_accessible :content
+
+  default_scope order: 'microposts.created_at DESC'
+
+  belongs_to :user
 
   validates :user_id, presence: true
+  validates :content, presence: true, length: {maximum: 140}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Microblog::Application.routes.draw do
 
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
 
   match "signin", to: "sessions#new"
   match "signout", to: "sessions#destroy", via: :delete
